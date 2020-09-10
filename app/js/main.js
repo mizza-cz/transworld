82d4e4c40b030db468f53c19bcd5206bbb19cb71
mizza-cz <mizza.cz666@gmail.com>
2020-09-10 21:39:18 +0200

    end  project

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+   $('.slider__inner').slick({
+      prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/prev.svg" alt="">Prev</button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.svg" alt="">Next</button>',
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+   });
+
+
+   $(".menu__burger").click(function () {
+      $(".menu").toggleClass("menu-open");
+   });
+});
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ gulp.task('sass', function () {
 
 gulp.task('style', function () {
    return gulp.src([
+      'node_modules/fancybox/dist/css/jquery.fancybox.css',
       'node_modules/normalize.css/normalize.css',
       'node_modules/slick-carousel/slick/slick.css',
       'node_modules/magnific-popup/dist/magnific-popup.css'
@@ -34,6 +35,7 @@ gulp.task('style', function () {
 
 gulp.task('script', function () {
    return gulp.src([
+      'node_modules/fancybox/dist/js/jquery.fancybox.js',
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
    ])
