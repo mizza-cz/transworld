8ca081f08b1616f37b33b06e0aa8947bab7b64cb
mizza-cz <mizza.cz666@gmail.com>
2020-11-17 20:42:13 +0100

    add fix animate

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   new WOW().init();
    $('.slider__inner').slick({
       prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/prev.svg" alt="">Prev</button>',
       nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.svg" alt="">Next</button>',
